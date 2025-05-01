e61d81fa063f6b1c0b98bc43f1d89eb1f1ea8dcf
Matheus Haig <matheushaigz09@gmail.com>
2025-05-01 16:25:31 -0300

    criado a controller aula 2

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/controller.java
@@ -0,0 +1,17 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping
+public class controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "essa e minha primeira mensagem nessa rota";
+    }
+}
