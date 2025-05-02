49528c7ca1eae43b1b40395c76b3c6f087e46128
Matheus Haig <matheushaigz09@gmail.com>
2025-05-02 14:58:08 -0300

    fix: coloca a coluna de email como unique

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,8 +27,12 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
     //  @ManyToOne um ninja tem uma unica missao
     @ManyToOne
